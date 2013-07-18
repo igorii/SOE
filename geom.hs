0946b44764eb1d9f6690d897649f127a19c6c2de
Tim Thornton <igorii@localhost.(none)>
2013-07-17 20:57:39 -0400

    Adding some attempts, having trouble with hugs

---
+++ geom.hs
@@ -0,0 +1,29 @@
+module Geom
+
+data Shape = Rectangle Side Side
+           | Ellipse Radius Radius
+           | RtTriangle Side Side
+           | Polygon [Vertex]
+    deriving Show
+
+triArea            :: Vertex -> Vertex -> Float
+triArea v1 v2 v3   = let a = distBetween v1 v2
+                         b = distBetween v2 v3
+                         c = distBetween v3 v1
+                         s = 0.5 * (a + b + c)
+                     in sqrt (s * (s - a) * (s - b) * (s - c))
+
+type Radius = Float
+type Side   = Float
+type Vertex = (Float, Float)
+
+area                      :: Shape -> Float
+area (Rectangle s1 s2)    = s1 * s2
+area (RtTriangle s1 s2)   = s1 * s2 / 2
+area (Ellipse r1 r2)      = pi * r1 * r2
+area (Polygon (v1 : vs))  = polyArea vs
+    where polyArea                 :: [Vertex] -> Float
+          polyArea (v2 : v3 : vs') = triArea v1 v2 v3
+                                        + polyArea (v3 : vs')
+          polyArea _               = 0
+                    
